d75662af8438b2fe9b517318d9cf80161efc1fbf
rene keijzer <renekeijzer1997@gmail.com>
2017-03-13 19:15:16 +0100

    response

--- Node Rest Service/routes/DatabaseConnection.js
+++ Node Rest Service/routes/DatabaseConnection.js
@@ -74,8 +74,8 @@ module.exports = {
                 todo.count({}, function(err, count){
                     var newTodo = new todo({userId:userId, note:todoString, todoId:count});
                     newTodo.save(function(err){
-                        if(err) return console.log(err);
-                        else return console.log(newTodo);
+                        if(err) callback(console.log(err));
+                        callback(console.log(newTodo));
 
                     })
                 });
